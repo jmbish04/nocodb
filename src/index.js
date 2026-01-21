d1b8c5646d36657db6ade7478b535ed312182731
jmbish04 <jmbish04@gmail.com>
2026-01-21 08:18:04 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,18 +5,27 @@ import { Client } from "pg";
 export class NocoDBContainer extends Container {
   defaultPort = 8080;
   sleepAfter = "10s";
-}
+  envVars = {
+    NC_DB: env.HYPERDRIVE.connectionString
+  };
 
-export class NocoDBDurableObject extends DurableObject {
-  constructor(ctx, env) {
-    super(ctx, env);
-  }
-}
+// export class NocoDBDurableObject extends DurableObject {
+//   constructor(ctx, env) {
+//     super(ctx, env);
+//   }
+// }
 
 export default {
   async fetch(request, env, ctx) {
     const container = env.NOCODB_CONTAINER.getByName("main-instance");
-    const response = await container.fetch(request);
+    await instanceOne.startAndWaitForPorts({
+        startOptions: {
+          envVars: {
+            NC_DB: env.HYPERDRIVE.connectionString,
+          },
+        },
+      });    
+  const response = await container.fetch(request);
 
     // Example Hyperdrive usage (external Postgres/MySQL required since disk is ephemeral)
     // const client = new Client({
